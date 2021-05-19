ae5c6b697f9f249a1776e6aec0159ac48a8276b2
joanaportugal <joanacportugal@gmail.com>
2021-05-20 00:17:33 +0100

    privação de rotas

--- js/controllers/UtilizadorController.js
+++ js/controllers/UtilizadorController.js
@@ -55,9 +55,32 @@ export default class UtilizadorController {
 
   checkRoute() {
     const publicRoutes = ["index", "login", "registo"];
+    const adminRoutes = [
+      "landing_admin",
+      "ver_utilizadores",
+      "gerir_atividades",
+      "gerir_medalhas",
+    ];
+    const userRoutes = [
+      "landing_user",
+      "perfil",
+      "jogos",
+      "jogo",
+      "estatisticas",
+      "trofeus",
+      "info",
+    ];
     const path = window.location.pathname;
     const file = path.substr(path.lastIndexOf("/") + 1);
     const route = file.split(".")[0];
+
+    if (
+      sessionStorage.getItem("active") == "undefined" &&
+      (adminRoutes.some((adminRoute) => adminRoute === route) ||
+        userRoutes.some((userRoute) => userRoute === route))
+    ) {
+      location.href = "../index.html";
+    }
 
     if (
       sessionStorage.getItem("active") != "undefined" &&
@@ -68,6 +91,21 @@ export default class UtilizadorController {
       } else if (sessionStorage.getItem("active") != undefined) {
         location.href = "./html/landing_user.html";
       }
+    }
+
+    if (
+      sessionStorage.getItem("active") == "admin" &&
+      userRoutes.some((userRoute) => userRoute === route)
+    ) {
+      location.href = "./landing_admin.html";
+    }
+
+    if (
+      sessionStorage.getItem("active") != "undefined" &&
+      sessionStorage.getItem("active") != "admin" &&
+      adminRoutes.some((adminRoute) => adminRoute === route)
+    ) {
+      location.href = "./landing_user.html";
     }
   }
 }
