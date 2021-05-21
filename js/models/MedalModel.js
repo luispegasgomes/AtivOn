f8d3db6d14d153be8f914898575f2605ec873c5e
ptnoob <72983038+ptnoob@users.noreply.github.com>
2021-05-21 14:27:45 +0100

    models

---
+++ js/models/MedalModel.js
@@ -0,0 +1,12 @@
+export default class MedalModel {
+    constructor(iconmedal, iconavatar, addamedal, addavatar, removemedal, removeavatar, namemedal, nameavatar) {
+        this.iconmedal = iconmedal;
+        this.iconavatar = iconavatar;
+        this.addmedal = addamedal;
+        this.addavatar = addavatar;
+        this.removemedal = removemedal;
+        this.removeavatar = removeavatar;
+        this.namemedal = namemedal;
+        this.nameavatar = nameavatar;
+    }
+}
---
+++ js/models/UserModel.js
@@ -0,0 +1,10 @@
+export default class UserModel {
+  constructor(name, username, email, password, birthdate, gender) {
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.birthdate = birthdate;
+    this.gender = gender;
+  }
+}
