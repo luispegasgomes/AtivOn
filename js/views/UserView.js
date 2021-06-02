d1c063e88566825ece8ad44fa481be4613d6c630
joanaportugal <joanacportugal@gmail.com>
2021-06-02 13:34:43 +0100

    display username and xp, update password

--- js/views/UserView.js
+++ js/views/UserView.js
@@ -8,9 +8,16 @@ export default class UserView {
     if (this.logoutBtn) {
       this.logout();
     }
-    //classe displayUsername - vai buscar o username ao sessionStorage
-    //classe displayXP - se não existir o atributo XP, vai aparecer "Nível 1 - 0 XP"
-    // chamar vars e função no construtor, fazer lógica no displayUserInfo()
+    this.displayUsername = document.getElementsByClassName("displayUsername");
+    this.displayXP = document.getElementsByClassName("displayXP");
+    if (this.displayUsername || this.displayXP) {
+      this.displayUserInfo();
+    }
+
+    this.profileForm = document.getElementById("profileForm");
+    if (this.profileForm) {
+      this.updatePassword();
+    }
   }
 
   checkLoggedUser() {
@@ -24,5 +31,44 @@ export default class UserView {
     });
   }
 
-  displayUserInfo()
+  displayUserInfo() {
+    const userInfo = JSON.parse(sessionStorage.getItem("active"));
+    for (const displayUsername of this.displayUsername) {
+      displayUsername.innerHTML = userInfo.username;
+    }
+    for (const displayXP of this.displayXP) {
+      displayXP.innerHTML = userInfo.xp ? `${userInfo.xp} XP` : `0 XP`;
+    }
+  }
+
+  updatePassword() {
+    this.profileForm.addEventListener("submit", (e) => {
+      const txtPassword = document.getElementById("txtPassword");
+      const txtConfPassword = document.getElementById("txtConfPassword");
+      const userInfo = JSON.parse(sessionStorage.getItem("active"));
+
+      if (txtPassword.value !== txtConfPassword.value) {
+        alert("As passwords são diferentes");
+      } else if (txtPassword.value === userInfo.password) {
+        alert("Não podes mudar para a mesma password");
+      } else {
+        sessionStorage.setItem(
+          "active",
+          JSON.stringify({ ...userInfo, password: txtPassword.value })
+        );
+        const userList = JSON.parse(localStorage.getItem("users"));
+        const newUserList = userList.map((userItem) =>
+          userItem.username === userInfo.username
+            ? { ...userItem, password: txtPassword.value }
+            : userItem
+        );
+        localStorage.setItem("users", JSON.stringify(newUserList));
+        alert("Password alterada com sucesso");
+        location.href = "./landing_user.html";
+      }
+
+      this.profileForm.reset();
+      e.preventDefault();
+    });
+  }
 }
