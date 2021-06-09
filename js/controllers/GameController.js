cbfc9b53c83818114714e73ea09eb085dea4021d
joanaportugal <joanacportugal@gmail.com>
2021-06-09 14:16:48 +0100

    completa game logic started

---
+++ js/controllers/GameController.js
@@ -0,0 +1,43 @@
+import StorageController from "./StorageController.js";
+
+export default class GameController {
+  constructor() {
+    this.userLogged = JSON.parse(sessionStorage.active);
+    this.userList = JSON.parse(localStorage.users);
+    this.activities = JSON.parse(localStorage.activities);
+    this.storageController = new StorageController();
+  }
+
+  updateXP(totalObtained) {
+    return {
+      ...this.userLogged,
+      xp: this.userLogged.xp
+        ? this.userLogged.xp + totalObtained
+        : totalObtained,
+    };
+  }
+
+  updateStorage() {
+    this.storageController.updateLocalStorage(
+      "users",
+      JSON.stringify(this.userList)
+    );
+    this.storageController.updateSessionStorage(
+      "active",
+      JSON.stringify(this.userLogged)
+    );
+  }
+
+  finishActivity(activity, counter) {
+    const activityXP =
+      activity == "quiz"
+        ? this.activities.quiz.activityXP
+        : this.activities.fillTheSpaces.activityXP;
+    const totalObtained = activityXP * counter;
+    this.userLogged = updateXP(totalObtained);
+    this.userList.map((user) =>
+      user.username === this.userLogged.username ? this.userLogged : user
+    );
+    updateStorage();
+  }
+}
--- js/views/GameView.js
+++ js/views/GameView.js
@@ -1,22 +1,40 @@
-import UserController from "../controllers/UserController.js";
-import StorageController from "../controllers/StorageController.js";
+import GameController from "../controllers/GameController.js";
 
 export default class GameView {
   constructor() {
-    this.userController = new UserController();
     this.gameNameCompleta = document.getElementById("gameNameCompleta");
     this.gameNameQuizOn = document.getElementById("gameNameQuizOn");
     this.showGamesName();
+    this.completaSend = document.getElementById("completaSend");
+    this.completaTratamento = document.getElementById("completaTratamento");
+    this.completaSintomas = document.getElementById("completaSintomas");
+    this.completaOMS = document.getElementById("completaOMS");
+    this.completaMascara = document.getElementById("completaMascara");
+    this.completaMetros = document.getElementById("completaMetros");
+    this.completaSNS24 = document.getElementById("completaSNS24");
+    this.completaFebre = document.getElementById("completaFebre");
+    this.completaDias = document.getElementById("completaDias");
+    if (this.completaSend) {
+      this.checkValues();
+    }
 
-    this.storageController = new StorageController();
+    this.gameController = new GameController();
   }
 
   showGamesName() {
     const gameInfo = JSON.parse(localStorage.getItem("activities"));
-    this.gameNameCompleta.innerHTML = gameInfo.fillTheSpaces.activityName;
-    this.gameNameQuizOn.innerHTML = gameInfo.quiz.activityName;
-
+    if (this.gameNameCompleta) {
+      this.gameNameCompleta.innerHTML = gameInfo.fillTheSpaces.activityName;
+    }
+    if (this.gameNameQuizOn) {
+      this.gameNameQuizOn.innerHTML = gameInfo.quiz.activityName;
+    }
   }
 
-  
+  checkValues() {
+    // eventListener de this.completaSend
+    // verificar valores dos inputs e somar +1 a cada valor certo
+    // no fim tirar comentário da seguinte linha (e meter dentro do eventListener):
+    // this.gameController.finishActivity("fillTheSpaces", counter)
+  }
 }
